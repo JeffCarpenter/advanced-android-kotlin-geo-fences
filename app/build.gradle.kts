6b575cd00205f1c26940c2546f34680eec3bb3a0
Jeff <pub@jeffc.ca>
2025-01-15 18:42:26 -0800

    fixup Gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            minifyEnabled = false
+            isMinifyEnabled = false
             proguardFiles(
                     getDefaultProguardFile("proguard-android-optimize.txt"),
                     "proguard-rules.pro"
